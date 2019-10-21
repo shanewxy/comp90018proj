0ff12a344f313e31401e3e117a4b4ed4a9d46273
shanewxy <592491808@qq.com>
2019-10-21 22:45:01 +1100

    navigation

--- app/src/main/java/com/unimelb/checkmein/MapsActivity.java
+++ app/src/main/java/com/unimelb/checkmein/MapsActivity.java
@@ -13,6 +13,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -118,7 +119,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             return Transaction.abort();
                         }
 
-
+                        double lat = 0;
+                        double log = 0;
+                        boolean rightTime = false;
                         Map<String, Session> sessionMap = subject.getSessions();
                         for (String key : sessionMap.keySet()) {
                             Session session = sessionMap.get(key);
@@ -128,23 +131,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             int day = session.day;
                             int currenthour = currentTime.getHours();
                             int currentday = currentTime.getDay();
-                            Log.d(TAG, "doTransaction: "+start);
-                            Log.d(TAG, "doTransaction: "+end);
-                            Log.d(TAG, "doTransaction: "+day);
-                            Log.d(TAG, "doTransaction: "+currentday);
-                            Log.d(TAG, "doTransaction: "+currenthour);
+
+                            LatLng target = new LatLng(building.getLatitude(), building.getLongitude());
+//                            mMap.addMarker(new MarkerOptions().position(target).title(subject.getName()));
                             float[] distance = new float[3];
                             Location.distanceBetween(building.latitude, building.longitude, lastLocation.getLatitude(), lastLocation.getLongitude(), distance);
-                            if (currentday == day && currenthour >= start && currenthour <= end && distance[0] < 100) {
-                                user.count += 1;
-                                user.date = today;
-                                students.put(getUid(), user);
-                                mutableData.setValue(subject);
-                                makeToast("Successfully checked in!");
-                                return Transaction.success(mutableData);
+                            if (currentday == day) {
+                                lat = building.latitude;
+                                log = building.longitude;
+                                if (currenthour >= start && currenthour <= end) {
+                                    rightTime = true;
+                                    if (distance[0] < 100) {
+                                        user.count += 1;
+                                        user.date = today;
+                                        students.put(getUid(), user);
+                                        mutableData.setValue(subject);
+                                        makeToast("Successfully checked in!");
+                                        return Transaction.success(mutableData);
+                                    }
+                                }
                             }
                         }
-                        makeToast("Fail! Wrong time or location!");
+                        if (lat == 0 && log == 0) {
+                            makeToast("Fail! Wrong day!");
+                            return Transaction.abort();
+                        }
+                        if (!rightTime) {
+                            makeToast("Fail! Not in class time!");
+                            return Transaction.abort();
+                        }
+                        Intent intent = new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("http://maps.google.com/maps?"
+                                        + "saddr=" + lastLocation.getLatitude() + "," + lastLocation.getLongitude()
+                                        + "&daddr=" + lat + "," + log
+                                        + "&avoid=highway"
+                                        + "&language=zh-CN")
+                        );
+
+                        intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                        startActivity(intent);
+                        makeToast("Fail! Location too far away!");
                         return Transaction.abort();
                     }
 
